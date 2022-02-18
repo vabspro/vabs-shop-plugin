51290b6d1394092f80375ebf4219c229d57beab2
uhorn <uwehornnet@users.noreply.github.com>
2022-02-18 10:47:00 +0100

    added tailwindcss

--- public/templates/shop.php
+++ public/templates/shop.php
@@ -5,6 +5,6 @@ $products = json_decode(file_get_contents($file), false);
 
 get_header(); ?>
 
-
+<div class="flex items-center"></div>
 
 <?php get_footer(); ?>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	mode: "jit",
+	content: ["./public/templates/*.php"],
+	theme: {
+		screens: {
+			tablet: "640px",
+			laptop: "1024px",
+			desktop: "1280px",
+		},
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+};
